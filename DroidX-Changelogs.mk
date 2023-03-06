dbe4ca9332b7a4d17b98a38d26ce9955e992b703
Noobie_ashwani <99090768+ashwani212@users.noreply.github.com>
2023-03-06 12:14:11 +0530

    DroidXUI: V1.0 Changelogs

---
+++ DroidX-Changelogs.mk
@@ -0,0 +1,15 @@
+======================================
+DroidX-UI v1.0 (Alpha One) Changelogs
+======================================
+
+- February security patch.
+- Switched to initial release v1.0.
+- DroidX-UI codename #CrackStone.
+- Added Support for Vanilla and Gapps variants.
+- "Customize" our hub is also included.
+- Customize: Added battery related customizations.
+- Customize: Added Fonts and icons related customizations.
+- Added Advance reboot feature.
+
+Note: Customize hub is keep on updating, So expect 
+the hub with full customizations in upcoming releases.
