75b2fb0cb0c8b5548d66de458c41aab69a5ea37b
ZirgomHaidar <53788144+ZirgomHaidar@users.noreply.github.com>
2023-09-16 11:17:44 +0530

    DroidXUI: v1.5.2 changelogs

--- DroidX-Changelogs.mk
+++ DroidX-Changelogs.mk
@@ -1,3 +1,42 @@
+============================
+DroidX-UI v1.5.2 Changelogs
+============================
+
+- Added toggle to switch between Android 11/Android 13 qs panel style. 
+- Added support for Face Unlock. 
+- Added customised animation on settings about dashboard.
+- Redesigned About Device section.
+- Added AOD, NFC Tile. 
+- Added customisations related to monet. 
+- Added smart pause feature. 
+- Added statusbar NFC icon. 
+- Integrated google lens into screenshot UI. 
+- Added feature to show bluetooth battery level. 
+- Added our own set of 22 droidx exclusive wallpapers. 
+- Added per app volume support. 
+- Added support for auto wifi and bluetooth off during no usage period. 
+- Added option for hotspot sharing over vpn. 
+- Added delete button on screenshots. 
+- Added UDFPS icons and animations.
+- Added pocket mode feature.
+- Added Pure Black Dark theme. 
+- Added multi-sim ringtone support.
+- Added new charging chip animation. 
+- Fixed issue related to mobile data and wifi tile. 
+- Added support for disable battery light. 
+- Added support to require unlocking to use sensitive Qs tiles. 
+- Implemented bluetooth dialog in QS. 
+- Fixed bug related to Android Auto. 
+- Added hotspot toggle in QS internet dialog.
+- Fixed a crash in suggestions option in launcher. 
+- Added media ripple effect. 
+- Added allow disabling power menu on secure lockscreen. 
+- Added toggle data usage view on QS footer. 
+- Added allow disabling QS on secure lockscreen. 
+- Added toggle for clock AM/PM style. 
+- Added Fast Charging Toggle for  Fast Charging supported devices
+
+
 ===========================
 DroidX-UI v1.5.1 Changelogs
 ===========================
